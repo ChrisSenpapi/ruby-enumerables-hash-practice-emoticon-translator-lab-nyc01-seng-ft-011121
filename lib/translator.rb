3f590f8cf4d1b1a00f6c60999faca6054865732d
Christopher Read <christopherread22@outlook.com>
2020-12-16 21:07:04 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -1,3 +1,14 @@
 require 'yaml'
 require 'pry'
 
+def load_library(emoticons)
+  new_hash = {}
+  emoticons = YAML.load_file("./lib/emoticons.yml")
+  emoticons.each do |meaning, emoticons|
+    new_hash["get_meaning"] = {} if new_hash["get_meaning"] == nil
+    new_hash["get_meaning"][emoticons[1]] = meaning
+    new_hash["get_emoticon"] = {} if new_hash["get_emoticon"] == nil
+    new_hash["get_emoticon"][emoticons[0]] = emoticons[1]
+  end
+  new_hash
+end	
