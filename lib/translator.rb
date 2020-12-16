4c74e5bd8a3b40f301343de83e8557d96277ef2a
Christopher Read <christopherread22@outlook.com>
2020-12-16 21:08:33 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -24,3 +24,12 @@ def get_japanese_emoticon(emoticons, emoticon)
   result
 end
 
+def get_english_meaning(emoticons, emoticon)
+  library = load_library(emoticons)
+  if library["get_meaning"].keys.include?(emoticon)
+    result = library["get_meaning"][emoticon]
+  else
+    result = "Sorry, that emoticon was not found"
+  end
+  result
+end
